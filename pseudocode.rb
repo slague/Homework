208bf848c2ca7be96c146498e498dea9b6ed9338
slague <stephanielague@gmail.com>
2017-01-24 09:09:58 -0700

    Initial commit

---
+++ pseudocode.rb
@@ -0,0 +1,17 @@
+# Begin by answering the phone saying 
+# HELLO, THIS IS A GROCERY STORE!
+# wait for a response
+# If the response has any lower case letters, respond with:
+# I AM HAVING A HARD TIME HEARING YOU.
+# If no response, respond with:
+# HELLO?!
+# If the response has all upper case letters, respond with:
+# NO, THIS IS NOT A PET STORE
+# If the response is:
+# GOODBYE!
+# and the caller has not said this yet, respond with:
+# ANYTHING ELSE I CAN HELP WITH?
+# If the response is
+# GOODBYE!
+# and the caller HAS said this before, respond with:
+# THANK YOU FOR CALLING!
